bf69b9b91d9400e1aed49e348e16177073643f2c
sankalp6115 <sankalpomar6115@gmail.com>
2026-06-29 00:56:21 +0530

    Bug fix

--- bootstrap/05-config.sh
+++ bootstrap/05-config.sh
@@ -13,7 +13,7 @@ cp -r config/sshd_config ~/ 2>/dev/null || true
 cp -r config/aliases ~/ 2>/dev/null || true
 cp -r config/motd ~/ 2>/dev/null || true
 
-cp audio/login.mp3 ~/system_scripts
+cp assets/audio/login.mp3 ~/system_scripts
 cp config/ssh_login.sh ~/system_scripts
 
 echo "Changing default shell to zsh..."
